ce01d78caa96ba02eb59fce43c0a354448cfb30d
Hebert Richard Masseno Dias <hebertdev82@gmail.com>
2023-01-25 19:06:01 -0300

    painel

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+# Alugue um Livro
+
+Projeto desenvolvido pela Susi Technologies (https://susitech.dev), usando React.Js [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Cadastro de Livros</h1>
+        <h3>Aqui cadastraremos os livros que temos em nossa biblioteca.</h3>
+        <br /><br />
+        <p>Campos que teremos: Nome do Livro, Autor, Ano de Publicação, Número de Edição, Editora, Palavras chave.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
